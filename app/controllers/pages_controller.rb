6856fd30bfa7991eddd3adcb9115a7891ddb69e8
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-10-17 10:54:22 +0200

    added map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
+
+
+    @markers =
+      [{
+        lat: 49.455861,
+        lng: 1.085160#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }]
   end
 end
